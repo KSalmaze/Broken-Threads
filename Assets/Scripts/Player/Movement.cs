b2eca9cae2833e76d33f6216e77918aaf092176a
Uber020 <renanhs1@hotmail.com>
2024-08-24 16:34:04 -0300

    commit final ???

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,35 +1,40 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [Header("Movement")]
-    public float maxSpeed = 5.0f;
+    private bool isDashing;
+    public float maxSpeed = 10f;
+    private float currentMaxSpeed = 10f;
     public float jumpForce = 5f;
     private float currentJumpForce=5;
     private float fallSpeed, hinput, vinput;
     private Rigidbody rb;
     private Vector3 moveDirection, currentSpeed;
-    public new Transform playerTransform;
-    
+    public Transform playerTransform;
+
     [Header("Ground check")]
     public LayerMask groundMask;
     public bool isGrounded, doubleJump;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         doubleJump = true;
     }
-    
+
     void Update()
     {
         hinput = Input.GetAxis("Horizontal");
         vinput = Input.GetAxis("Vertical");  //raycast pra ver se encosta no chao \ maxDistance=altura/2
-        isGrounded = Physics.Raycast(playerTransform.position, Vector3.down, 1.1f, groundMask);
+        isGrounded = Physics.Raycast(playerTransform.position, Vector3.down, 1.05f, groundMask);
         rb.drag = isGrounded ? 2f : 0f;
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing) StartCoroutine(Dash());
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isGrounded)
@@ -47,29 +52,46 @@ public class Movement : MonoBehaviour
                 rb.AddForce(Vector3.up * currentJumpForce, ForceMode.Impulse);
             }
         }
-        
+
         if (!isGrounded && (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.LeftAlt)))
         {
             rb.velocity -= new Vector3(0f, rb.velocity.y, 0f);
             rb.AddForce(Vector3.down * (5 * jumpForce), ForceMode.Impulse);
             StartCoroutine(JumpHigher());
         }
-        
+
         //input.normalize \ salva a V(y) \ "acelera" o player limita a velocidade pra maxSpeed \ restaura o V(y)
         moveDirection= (playerTransform.right * hinput + playerTransform.forward * vinput).normalized;
         fallSpeed = rb.velocity.y;
         currentSpeed = rb.velocity;
         currentSpeed.y = 0;
-        currentSpeed += moveDirection * maxSpeed /5;
-        currentSpeed = Vector3.ClampMagnitude(currentSpeed, maxSpeed);
+        currentSpeed *= 0.8f; //deixa o movimento mais controlavel
+        currentSpeed += moveDirection * currentMaxSpeed /5;
+        currentSpeed = Vector3.ClampMagnitude(currentSpeed, currentMaxSpeed);
         currentSpeed.y = fallSpeed;
         rb.velocity = currentSpeed;
     }
-    
+
     IEnumerator JumpHigher()
     {
         currentJumpForce = 2*jumpForce;
         yield return new WaitForSecondsRealtime(0.5f);
         currentJumpForce = jumpForce;
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        currentMaxSpeed = 5*maxSpeed;
+        Vector3 forceDirection = currentSpeed.normalized * currentMaxSpeed;
+        forceDirection.y = 0;
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        while (currentMaxSpeed > maxSpeed)
+        {
+            currentMaxSpeed -= 3f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1.5f); //cooldown do dash
+        isDashing = false;
     }
 }
